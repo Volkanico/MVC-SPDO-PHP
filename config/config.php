cb3dd88dcf13554904a457d80b4533d0fc0ffe90
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2022-11-15 20:59:55 +0100

    Inici segon repositori per passar-ho a net. Plantilla sencilla acabada porfin

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "mvc_example");
+define("DB_USER", "Admin");
+define("DB_PASS", "Admin");
+
+/* Default options */
+define("DEFAULT_CONTROLLER", "note");
+define("DEFAULT_ACTION", "list");
+
+?>
